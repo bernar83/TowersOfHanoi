b9fc20eddd91c43b3302a5f45d88bc7fa514d2a0
Adrian Bernardo <nabernardo93@gmail.com>
2017-12-10 21:31:48 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TowersOfHanoi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isNumber = false;
+            int number = 0;
+            while (!isNumber)
+            {
+                Console.Write("Enter number of discs: ");
+                var input = Console.ReadLine();
+                isNumber = int.TryParse(input, out number);
+            }
+
+
+            Console.ReadKey();
+        }
+        
+    }
+}
