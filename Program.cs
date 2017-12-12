bb56a05561418ac5cffd579427ccd4eb2a907c52
Adrian Bernardo <nabernardo93@gmail.com>
2017-12-12 15:59:55 -0800

    Add Move, ShowStacks, AddDiscs, and AskForDiscs methods

--- Program.cs
+++ Program.cs
@@ -10,6 +10,67 @@ namespace TowersOfHanoi
     {
         static void Main(string[] args)
         {
+
+
+            int number = AskForDiscs();
+
+            Stack<int> from = new Stack<int>();
+            Stack<int> to = new Stack<int>();
+            Stack<int> aux = new Stack<int>();
+            AddDiscs(number, from);
+
+            Move(number, from, aux, to);
+
+            Console.ReadKey();
+        }
+
+        // from, aux, to
+
+        private static void Move(int number, params Stack<int>[] args)
+        {
+            if (number > 0)
+            {
+                ShowStacks(args);
+
+                Move(number - 1, args[0], args[2], args[1]);
+
+                int movedDisc = args[0].Pop();
+                args[2].Push(movedDisc);
+
+                ShowStacks(args[0], args[1], args[2]);
+
+                Move(number - 1, args[1], args[0], args[2]);
+            }
+        }
+
+        private static void ShowStacks(params Stack<int>[] args)
+        {
+            for (var i = 0; i < args.Length; i++)
+            {
+                if (args[i].Count > 0)
+                {
+                    foreach (var disc in args[i])
+                    {
+                        Console.Write(disc);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine();
+                }
+            }
+        }
+
+        private static void AddDiscs(int number, Stack<int> pole)
+        {
+            for (var i = 1; i <= number; i++)
+            {
+                pole.Push(i);
+            }
+        }
+
+        private static int AskForDiscs()
+        {
             bool isNumber = false;
             int number = 0;
             while (!isNumber)
@@ -18,10 +79,7 @@ namespace TowersOfHanoi
                 var input = Console.ReadLine();
                 isNumber = int.TryParse(input, out number);
             }
-
-
-            Console.ReadKey();
+            return number;
         }
-        
     }
 }
