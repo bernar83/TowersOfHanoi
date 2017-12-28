20f5d923b2e7c0a07e3db9035d0147c733d82efb
Adrian Bernardo <nabernardo93@gmail.com>
2017-12-28 00:22:48 -0800

    Add new class Hanoi

---
+++ Hanoi.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TowersOfHanoi
+{
+    class Hanoi
+    {
+        private List<int> _from = new List<int>();
+        private List<int> _to = new List<int>();
+        private List<int> _aux = new List<int>();
+    }
+}
