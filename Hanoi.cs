56a003ed57aba1a1419039a68079e4fa9ae21aa8
Adrian Bernardo <nabernardo93@gmail.com>
2017-12-31 22:15:18 -0800

    Move methods from Program class and change lists to stacks

--- Hanoi.cs
+++ Hanoi.cs
@@ -8,8 +8,60 @@ namespace TowersOfHanoi
 {
     class Hanoi
     {
-        private List<int> _from = new List<int>();
-        private List<int> _to = new List<int>();
-        private List<int> _aux = new List<int>();
+        private Stack<int> from = new Stack<int>();
+        private Stack<int> to = new Stack<int>();
+        private Stack<int> aux = new Stack<int>();
+        private int number;
+
+        public Hanoi(int number)
+        {
+            this.number = number;
+        }
+
+        public void Move()
+        {
+           if (number == 1)
+           {
+                to.Push(from.Pop());
+                ShowStacks();
+                return;
+           }
+
+
+        }
+
+        // Integer, Stack -> Stack
+        // Add number of discs to from pole
+        public void AddDiscs()
+        {
+            for (var i = 1; i <= number; i++)
+            {
+                from.Push(i);
+            }
+        }
+
+        // Stack, Stack, Stack -> Stack, Stack, Stack
+        // Print the amount of discs to console
+        public void ShowStacks()
+        {
+            Stack<int>[] poles = new Stack<int>[] { from, aux, to };
+            for (int i = 0; i < 3; i++)
+            {
+                Stack<int> selectedPole = poles[i];
+                if (selectedPole.Count == 0)
+                {
+                    Console.WriteLine();
+                }
+                else
+                {
+                    foreach (int disc in selectedPole)
+                    {
+                        Console.Write(disc);
+                    }
+                    Console.WriteLine();
+                }
+            }
+            Console.WriteLine("============================================");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -26,65 +26,10 @@ namespace TowersOfHanoi
         {
             int number = AskForDiscs();     // Integer -> Integer
 
-            List<int> from = new List<int>();
-            List<int> to = new List<int>();
-            List<int> aux = new List<int>();
-            AddDiscs(number, from);         // Integer, List -> List
             ShowStacks(from, aux, to);      // List, List, List -> List, List, List
             Move(number, from, aux, to);    // Integer, List, List, List 
 
             Console.ReadKey();
-        }
-
-        private static void Move(int number, List<int> from, List<int> aux, List<int> to)
-        {
-            if (number > 0)
-            {
-                Move(number - 1, from, to, aux);
-
-                int discToRemove = from.Count - 1;
-                int removedDisc = from[discToRemove];
-                from.Remove(removedDisc);
-                to.Add(removedDisc);
-
-                ShowStacks(from, aux, to);
-
-                Move(number - 1, aux, from, to);
-            }
-        }
-
-        // List, List, List -> List, List, List
-        // Print the amount of discs to console
-        private static void ShowStacks(List<int> from, List<int> aux, List<int> to)
-        {
-            List<int>[] poles = new List<int>[] { from, aux, to };
-            for (int i = 0; i < 3; i++)
-            {
-                List<int> selectedPole = poles[i];
-                if (selectedPole.Count == 0)
-                {
-                    Console.WriteLine();
-                }
-                else
-                {
-                    foreach (int disc in selectedPole)
-                    {
-                        Console.Write(disc);
-                    }
-                    Console.WriteLine();
-                }
-            }
-            Console.WriteLine("============================================");
-        }
-
-        // Integer, List -> List
-        // Add number of discs to pole
-        private static void AddDiscs(int number, List<int> pole)
-        {
-            for (var i = number; i > 0; i--)
-            {
-                pole.Add(i);
-            }
         }
 
         // Integer -> Integer
